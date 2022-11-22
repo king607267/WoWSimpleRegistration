c1b120596d5954e900f54d1f5e3266d956c81b0e
wangjueming <wangjueming@ule.com>
2022-11-22 17:40:12 +0800

    delete footer.php,header.php

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
  * Email : lichwow_masterking@yahoo.com
  * Date: 11/26/2018 - 8:36 PM
  */
-require_once 'header.php';
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="MasterkinG32.CoM"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="description" content="Index">
+    <meta name="description" content="WJMWoW">
+    <title>WJMWoW</title>
+    <link rel="stylesheet"
+          href="./css/bootstrap.min.css">
+    <link rel="stylesheet"
+          href="./css/style.css">
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+</head>
+<body>
+<div class="container">
 <div class="row">
     <div class="main-box">
         <img src="images/wow-logo.png">
@@ -29,4 +48,9 @@ require_once 'header.php';
         </div>
     </div>
 </div>
-<?php require_once 'footer.php'; ?>
+</div>
+<footer class="text-center">
+    <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=TuCwkQcKkc4Pz57ii60Fma91Lwvsht77&jump_from=webapi">魔兽闲聊学习讨论群 745274862</a>
+</footer>
+</body>
+</html>
