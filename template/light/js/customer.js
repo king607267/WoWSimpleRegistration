0036f7373b14b287bf632a3d4ec66f4dc760e371
king607267 <50839213@qq.com>
2022-11-22 22:15:47 +0800

    modify customer.js

--- template/light/js/customer.js
+++ template/light/js/customer.js
@@ -2,7 +2,7 @@ $(function () {
     setInterval(function () {
         let href = $(".nav-item.nav-link.active.show").attr("href");
         if (href == "#nav-serverstatus") {
-            window.location.reload();
+            window.location.href = location.href;
         }
     }, 60000);
 })
